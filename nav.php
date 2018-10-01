0b4dee0e0b9499c729df1af67652b36072158a90
melissa-anderson <ande0524@algonquinlive.com>
2018-10-01 08:44:20 -0400

    Initial commit

---
+++ nav.php
@@ -0,0 +1,20 @@
+<?php
+// associative array
+$links = [
+'Home' => 'index.php',
+'About' => 'about.php',
+'Work' => 'work.php'
+];
+?>
+
+<nav> 
+  <ul>
+    <?php foreach ($links as $title => $href) : ?>
+    <li>
+    <a href="<?php echo $href; ?>">
+    <?php echo $title; ?>
+    </a>
+    </li>
+    <?php endforeach; ?>
+  </ul>
+</nav>
